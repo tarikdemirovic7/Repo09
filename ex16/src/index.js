4e842afce262245226d4f4d9480212fdb977c606
TA <TA@TAs-iMac.local>
2021-09-21 15:21:09 +0200

    JS project Ternary Operator

---
+++ ex16/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+function checkSign(num) {
+    return (num < 0) ? "negative"
+        : (num > 0) ? "positive"
+        : "zero";
+}
+console.log(checkSign(10));
+console.log(checkSign(-12));
+console.log(checkSign(0));
+// Only change code above this line
+module.exports = checkSign;
