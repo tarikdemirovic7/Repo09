1ab9afe76e0af04a255be459e5ee883b0787e863
TA <TA@TAs-iMac.local>
2021-09-21 15:03:25 +0200

    JS project Identical Options in Switch Statements

---
+++ ex12/src/index.js
@@ -0,0 +1,35 @@
+function switchCaseSequential(num) {
+    var answer;
+    // Write code below this line
+    switch (num) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
+        
+    }
+    // Write code above this line
+    return answer;
+}
+
+console.log(switchCaseSequential(1));
+console.log(switchCaseSequential(2));
+console.log(switchCaseSequential(3));
+console.log(switchCaseSequential(4));
+console.log(switchCaseSequential(5));
+console.log(switchCaseSequential(6));
+console.log(switchCaseSequential(7));
+console.log(switchCaseSequential(8));
+console.log(switchCaseSequential(9));
+module.exports = switchCaseSequential;
