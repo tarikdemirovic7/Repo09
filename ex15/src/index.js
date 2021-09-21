61529f5438b863faea68943d0e9ea0c8a162a463
TA <TA@TAs-iMac.local>
2021-09-21 15:17:59 +0200

    JS project Do...While Loops

---
+++ ex15/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function myDoWhile() {
+    var myNumbers = "", i = 0;
+    do {
+        myNumbers += i;
+        if (i != 9) {
+            myNumbers += ", ";
+        }
+        i++;
+    } while (i < 10);
+    return myNumbers;
+}
+console.log(myDoWhile());
+// Only change code above this line
+module.exports = myDoWhile;
