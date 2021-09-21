89a622f746c18e61fad790f0ae511ef928d919b3
TA <TA@TAs-iMac.local>
2021-09-21 15:06:34 +0200

    JS project While Loops

---
+++ ex13/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function inverseWhile() {
+    var fiveNumbers = "", i = "5";
+    while (i > -1) {
+        fiveNumbers += i;
+        if (i!=0) {
+            fiveNumbers += ",";
+        }
+        i--;
+    }
+    return fiveNumbers;
+}
+// Only change code above this line
+console.log(inverseWhile());
+module.exports = inverseWhile;
