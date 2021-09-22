2cc03755116c4dce03bded8ad218460fedfb7360
TA <TA@TAs-iMac.local>
2021-09-22 09:16:31 +0200

    JS project Comparison With Operators

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,15 +1,15 @@
 // Only change code below this line
 function comparisonToEqual(a) {
-    if (a<5) {
+    if (a < 5) {
         return "Less than 5";
     }
-    if (a<10) {
+    if (a < 10) {
         return "Less than 10";
     }
-    if (a>20) {
+    if (a > 20) {
         return "More than 20";
     }
-    if (a>=10) {
+    if (a >= 10) {
         return "10 or over";
     }
 }
